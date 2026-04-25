6f2a0e48b0735b132070af31dc6568bc2a93a085
Yurii Korolkov <uriy1uriy@gmail.com>
2026-04-25 23:41:13 +0300

    Prepare UCF smoke test for DeepfakeBench

--- training/test.py
+++ training/test.py
@@ -1,79 +1,65 @@
 """
-eval pretained model.
+eval pretrained model.
 """
 import os
 import numpy as np
-from os.path import join
-import cv2
 import random
-import datetime
-import time
 import yaml
-import pickle
 from tqdm import tqdm
-from copy import deepcopy
-from PIL import Image as pil_image
+
 from metrics.utils import get_test_metrics
 import torch
-import torch.nn as nn
-import torch.nn.parallel
 import torch.backends.cudnn as cudnn
-import torch.nn.functional as F
 import torch.utils.data
-import torch.optim as optim
 
 from dataset.abstract_dataset import DeepfakeAbstractBaseDataset
-from dataset.ff_blend import FFBlendDataset
-from dataset.fwa_blend import FWABlendDataset
-from dataset.pair_dataset import pairDataset
-
-from trainer.trainer import Trainer
 from detectors import DETECTOR
-from metrics.base_metrics_class import Recorder
-from collections import defaultdict
 
 import argparse
-from logger import create_logger
 
 parser = argparse.ArgumentParser(description='Process some paths.')
-parser.add_argument('--detector_path', type=str, 
-                    default='/home/zhiyuanyan/DeepfakeBench/training/config/detector/resnet34.yaml',
-                    help='path to detector YAML file')
+parser.add_argument(
+    '--detector_path',
+    type=str,
+    default='/home/zhiyuanyan/DeepfakeBench/training/config/detector/resnet34.yaml',
+    help='path to detector YAML file'
+)
 parser.add_argument("--test_dataset", nargs="+")
-parser.add_argument('--weights_path', type=str, 
-                    default='/mntcephfs/lab_data/zhiyuanyan/benchmark_results/auc_draw/cnn_aug/resnet34_2023-05-20-16-57-22/test/FaceForensics++/ckpt_epoch_9_best.pth')
-#parser.add_argument("--lmdb", action='store_true', default=False)
+parser.add_argument(
+    '--weights_path',
+    type=str,
+    default='/mntcephfs/lab_data/zhiyuanyan/benchmark_results/auc_draw/cnn_aug/resnet34_2023-05-20-16-57-22/test/FaceForensics++/ckpt_epoch_9_best.pth'
+)
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def init_seed(config):
     if config['manualSeed'] is None:
         config['manualSeed'] = random.randint(1, 10000)
     random.seed(config['manualSeed'])
     torch.manual_seed(config['manualSeed'])
-    if config['cuda']:
+    if config['cuda'] and torch.cuda.is_available():
         torch.cuda.manual_seed_all(config['manualSeed'])
 
 
 def prepare_testing_data(config):
     def get_test_data_loader(config, test_name):
-        # update the config dictionary with the specific testing dataset
-        config = config.copy()  # create a copy of config to avoid altering the original one
-        config['test_dataset'] = test_name  # specify the current test dataset
+        config = config.copy()
+        config['test_dataset'] = test_name
         test_set = DeepfakeAbstractBaseDataset(
-                config=config,
-                mode='test', 
-            )
-        test_data_loader = \
-            torch.utils.data.DataLoader(
-                dataset=test_set, 
-                batch_size=config['test_batchSize'],
-                shuffle=False, 
-                num_workers=int(config['workers']),
-                collate_fn=test_set.collate_fn,
-                drop_last=False
-            )
+            config=config,
+            mode='test',
+        )
+        test_data_loader = torch.utils.data.DataLoader(
+            dataset=test_set,
+            batch_size=config['test_batchSize'],
+            shuffle=False,
+            num_workers=int(config['workers']),
+            collate_fn=test_set.collate_fn,
+            drop_last=False
+        )
         return test_data_loader
 
     test_data_loaders = {}
@@ -89,55 +75,113 @@ def choose_metric(config):
     return metric_scoring
 
 
+def _extract_prob_tensor(predictions):
+    """
+    Normalize model outputs so test.py can work with different detector schemas.
+    Supports keys like: prob / cls / pred / logits
+    """
+    if isinstance(predictions, torch.Tensor):
+        prob_tensor = predictions
+    elif isinstance(predictions, dict):
+        if 'prob' in predictions:
+            prob_tensor = predictions['prob']
+        elif 'cls' in predictions:
+            prob_tensor = predictions['cls']
+        elif 'pred' in predictions:
+            prob_tensor = predictions['pred']
+        elif 'logits' in predictions:
+            prob_tensor = predictions['logits']
+        else:
+            raise KeyError(f"Model output keys do not include prob/cls/pred/logits: {list(predictions.keys())}")
+    else:
+        raise TypeError(f"Unsupported prediction output type: {type(predictions)}")
+
+    if not torch.is_tensor(prob_tensor):
+        prob_tensor = torch.tensor(prob_tensor)
+
+    # Convert logits / 2-class outputs to positive-class probability-like score
+    if prob_tensor.ndim > 1:
+        if prob_tensor.shape[-1] == 1:
+            prob_tensor = prob_tensor.squeeze(-1)
+        else:
+            prob_tensor = torch.softmax(prob_tensor, dim=1)[:, 1]
+
+    return prob_tensor
+
+
+def _extract_feat_tensor(predictions):
+    """
+    Optional feature extractor. If absent, return None.
+    """
+    if isinstance(predictions, dict):
+        for key in ['feat', 'feature', 'features', 'embedding', 'emb']:
+            if key in predictions:
+                feat_tensor = predictions[key]
+                if torch.is_tensor(feat_tensor):
+                    return feat_tensor
+                return torch.tensor(feat_tensor)
+    return None
+
+
 def test_one_dataset(model, data_loader):
     prediction_lists = []
     feature_lists = []
     label_lists = []
-    for i, data_dict in tqdm(enumerate(data_loader), total=len(data_loader)):
-        # get data
-        data, label, mask, landmark = \
-        data_dict['image'], data_dict['label'], data_dict['mask'], data_dict['landmark']
+
+    for _, data_dict in tqdm(enumerate(data_loader), total=len(data_loader)):
+        data, label, mask, landmark = (
+            data_dict['image'],
+            data_dict['label'],
+            data_dict['mask'],
+            data_dict['landmark']
+        )
+
         label = torch.where(data_dict['label'] != 0, 1, 0)
-        # move data to GPU
+
         data_dict['image'], data_dict['label'] = data.to(device), label.to(device)
         if mask is not None:
             data_dict['mask'] = mask.to(device)
         if landmark is not None:
             data_dict['landmark'] = landmark.to(device)
 
-        # model forward without considering gradient computation
         predictions = inference(model, data_dict)
-        label_lists += list(data_dict['label'].cpu().detach().numpy())
-        prediction_lists += list(predictions['prob'].cpu().detach().numpy())
-        feature_lists += list(predictions['feat'].cpu().detach().numpy())
-    
-    return np.array(prediction_lists), np.array(label_lists),np.array(feature_lists)
-    
-def test_epoch(model, test_data_loaders):
-    # set model to eval mode
-    model.eval()
 
-    # define test recorder
+        prob_tensor = _extract_prob_tensor(predictions)
+        feat_tensor = _extract_feat_tensor(predictions)
+
+        label_lists += list(data_dict['label'].cpu().detach().numpy())
+        prediction_lists += list(prob_tensor.cpu().detach().numpy())
+
+        if feat_tensor is not None:
+            feature_lists += list(feat_tensor.cpu().detach().numpy())
+
+    feat_array = np.array(feature_lists) if len(feature_lists) > 0 else None
+    return np.array(prediction_lists), np.array(label_lists), feat_array
+
+
+def test_epoch(model, test_data_loaders):
+    model.eval()
     metrics_all_datasets = {}
 
-    # testing for all test data
     keys = test_data_loaders.keys()
     for key in keys:
         data_dict = test_data_loaders[key].dataset.data_dict
-        # compute loss for each dataset
-        predictions_nps, label_nps,feat_nps = test_one_dataset(model, test_data_loaders[key])
-        
-        # compute metric for each dataset
-        metric_one_dataset = get_test_metrics(y_pred=predictions_nps, y_true=label_nps,
-                                              img_names=data_dict['image'])
+
+        predictions_nps, label_nps, feat_nps = test_one_dataset(model, test_data_loaders[key])
+
+        metric_one_dataset = get_test_metrics(
+            y_pred=predictions_nps,
+            y_true=label_nps,
+            img_names=data_dict['image']
+        )
         metrics_all_datasets[key] = metric_one_dataset
-        
-        # info for each dataset
+
         tqdm.write(f"dataset: {key}")
         for k, v in metric_one_dataset.items():
             tqdm.write(f"{k}: {v}")
 
     return metrics_all_datasets
+
 
 @torch.no_grad()
 def inference(model, data_dict):
@@ -146,50 +190,46 @@ def inference(model, data_dict):
 
 
 def main():
-    # parse options and load config
     with open(args.detector_path, 'r') as f:
         config = yaml.safe_load(f)
+
     with open('./training/config/test_config.yaml', 'r') as f:
         config2 = yaml.safe_load(f)
+
     config.update(config2)
     if 'label_dict' in config:
-        config2['label_dict']=config['label_dict']
+        config2['label_dict'] = config['label_dict']
+
     weights_path = None
-    # If arguments are provided, they will overwrite the yaml settings
+
     if args.test_dataset:
         config['test_dataset'] = args.test_dataset
+
     if args.weights_path:
         config['weights_path'] = args.weights_path
         weights_path = args.weights_path
-    
-    # init seed
+
     init_seed(config)
 
-    # set cudnn benchmark if needed
-    if config['cudnn']:
+    if config['cudnn'] and device.type == 'cuda':
         cudnn.benchmark = True
 
-    # prepare the testing data loader
     test_data_loaders = prepare_testing_data(config)
-    
-    # prepare the model (detector)
+
     model_class = DETECTOR[config['model_name']]
     model = model_class(config).to(device)
-    epoch = 0
+
     if weights_path:
-        try:
-            epoch = int(weights_path.split('/')[-1].split('.')[0].split('_')[2])
-        except:
-            epoch = 0
         ckpt = torch.load(weights_path, map_location=device)
         model.load_state_dict(ckpt, strict=True)
         print('===> Load checkpoint done!')
     else:
         print('Fail to load the pre-trained weights')
-    
-    # start testing
+
     best_metric = test_epoch(model, test_data_loaders)
     print('===> Test Done!')
+    print(best_metric)
+
 
 if __name__ == '__main__':
     main()
